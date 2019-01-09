1a1a7701128ac02d2293a1db68ff77f3f49e9bca
AsiaKK <joanna.kaminska83@gmail.com>
2019-01-09 21:40:52 +0100

    commit po drugiej zmianie

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("i robię zadanie domowe");
     }
+
 }
